54721f49df4f094c68314050fa152b6f01718688
Mamun <shahriyar.m.mamun@gmail.com>
2021-03-17 00:45:16 +0600

    all working

---
+++ README.md
@@ -0,0 +1,23 @@
+# Fire Alarm System -> Text & Call
+
+## Introduction
+
+> This was a fun project which I did at the very beginning of my university life. 
+
+## Features
+
+> 1. Detect leakage of gas and smoke
+> 2. Send sms/call alert to the house owner
+> 3. Starts buzzing when detects gas or smoke
+
+## Components:
+
+> 1. arduino uno
+> 2. gas & smoke detector sensor
+> 3. gsm module
+> 4. breadboard , buzzer, light, wires etc
+
+## Working Demo: 
+
+video: https://www.youtube.com/embed/ZxrcgKaeaGE"
+# fire-alarm-system-arduino
---
+++ mainCode.c
@@ -0,0 +1,132 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(9, 10);
+char msg;
+char call;
+int redLed = 12;
+int greenLed = 11;
+int buzzer = 10; //buzzer 
+int smokeA0 = A5;
+int sensorThres = 500;
+
+void setup()
+{
+  mySerial.begin(9600);   // Setting the baud rate of GSM Module  
+  Serial.begin(9600);    // Setting the baud rate of Serial Monitor (Arduino)
+  Serial.println("GSM SIM900A BEGIN");
+  Serial.println("Enter character for control option:");
+  Serial.println("h : to disconnect a call");
+  Serial.println("i : to receive a call");
+  Serial.println("s : to send message");
+  Serial.println("c : to make a call");  
+  Serial.println("e : to redial");
+  Serial.println();
+  delay(100);
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(smokeA0, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{  
+  if (Serial.available()>0)
+   switch(Serial.read())
+  {
+    case 's':
+      SendMessage();
+      break;
+    case 'c':
+      MakeCall();
+      break;
+    case 'h':
+      HangupCall();
+      break;
+    case 'e':
+      RedialCall();
+      break;
+    case 'i':
+      ReceiveCall();
+      break;
+  }
+ if (mySerial.available()>0)
+ Serial.write(mySerial.read());
+
+ int analogSensor = analogRead(smokeA0);
+
+  Serial.print("Pin A0: ");
+  Serial.println(analogSensor);
+
+  if (analogSensor > sensorThres)
+  {
+    SendMessage();
+    digitalWrite(redLed, HIGH);
+    digitalWrite(greenLed, LOW);
+    tone(buzzer, 1000, 200);
+    
+  }
+  else
+  {
+    digitalWrite(redLed, LOW);
+    digitalWrite(greenLed, HIGH);
+    noTone(buzzer);
+  }
+  delay(100);
+
+ 
+}
+
+void SendMessage()
+{
+  mySerial.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
+  delay(1000);  // Delay of 1000 milli seconds or 1 second
+  mySerial.println("AT+CMGS=\"+8801xxxxxxxxx\"\r"); // Replace x with mobile number
+  delay(1000);
+  mySerial.println("TEST");// The SMS text you want to send
+  delay(100);
+   mySerial.println((char)26);// ASCII code of CTRL+Z
+  delay(1000);
+}
+
+void ReceiveMessage()
+{
+  mySerial.println("AT+CNMI=2,2,0,0,0"); // AT Command to recieve a live SMS
+  delay(1000);
+  if (mySerial.available()>0)
+  {
+    msg=mySerial.read();
+    Serial.print(msg);
+  }
+}
+
+void MakeCall()
+{
+  mySerial.println("ATD+60XXXXXXXXX;"); // ATDxxxxxxxxxx; -- watch out here for semicolon at the end!!
+  Serial.println("Calling  "); // print response over serial port
+  delay(1000);
+}
+
+
+void HangupCall()
+{
+  mySerial.println("ATH");
+  Serial.println("Hangup Call");
+  delay(1000);
+}
+
+void ReceiveCall()
+{
+  mySerial.println("ATA");
+  delay(1000);
+  {
+    call=mySerial.read();
+    Serial.print(call);
+  }
+}
+
+void RedialCall()
+{
+  mySerial.println("ATDL");
+  Serial.println("Redialing");
+  delay(1000);
+}
